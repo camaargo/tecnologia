a16df2d240a3b6bb8723f7679dfe4f6b6543d432
camaargo <159158804+camaargo@users.noreply.github.com>
2024-11-29 20:27:47 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,1 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'É uma linguagem de programação. '
+)
 
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Uma variável é um espaço na memória que armazena dados que podem mudar. '
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa específica e pode ser reutilizado. '
+)
+
+criaCartao(
+    'Programação',
+    'O que é um algoritmo?',
+    'Um algoritmo é uma sequência de instruções passo a passo para resolver um problema.'
+)
